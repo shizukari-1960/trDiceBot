cfd96e52663cd2bd83db36da31cc47cf5abbc1c5
shizukari-1960 <shizukari1960@gmail.com>
2026-06-17 00:37:03 +0800

    minor change

--- api_cnt.py
+++ api_cnt.py
@@ -1,37 +1,46 @@
-import requests
+import aiohttp
+import asyncio
 
-url = 'http://localhost:9292'
+url = 'https://bcdice.onlinesession.app/'
 
 system_alias = {
     "D":"DiceBot",
     "d":"DiceBot",
     "sw": "SwordWorld2.5:SimplifiedChinese",
-    "dx": 'DoubleCross'
+    "dx": 'DoubleCross',
+    "sg": "ShinobiGami",
+    "SG": "ShinobiGami"
 }
 
-def roll_dice(sys:str, cmd:str) -> str:
-    
+async def roll_dice_async(sys:str, cmd:str) -> str:
+    """
+    :param sys: System code provide by bcdice API.
+    :type sys: str
+    :param cmd: Diceroll command.
+    :type cmd: str
+    :return: Roll result.
+    :rtype: str
+    """
     endpoint = f'{url}/v2/game_system/{system_alias[sys] if sys in system_alias.keys() else sys}/roll'
-    params = {
+    
+    async with aiohttp.ClientSession() as session:
+        params = {
         'command': cmd
-    }
+        }
+        try:
+            async with session.get(endpoint, params=params, raise_for_status=True) as response:
+                response.raise_for_status()
+                data = await response.json()
 
-    try:
-        response = requests.get(endpoint, params=params)
-        response.raise_for_status()
-        data = response.json()
-
-        if data.get('ok'):
-            return data['text']
-            
-        else:
-            return data.get('reason')
-        
-        
-    except requests.exceptions.RequestException as e:
-        print('Api fail:', e)
-        return None
+                if data.get('ok'):
+                    return data['text']
+                else:
+                    return data.get('reason')
+        except aiohttp.ClientResponseError as e:
+            print('API Fail:', e)
+            return None
 
 
 if __name__ == '__main__':
-    roll_dice('sw','k100@10+7')
+    ct = asyncio.run(roll_dice_async('sw','k100@10+7'))
+    print(ct)
--- main.py
+++ main.py
@@ -6,9 +6,8 @@ from dotenv import load_dotenv
 
 import asyncio
 import re
+from random import choice, randint
 
-import tools
-import sw
 import inf
 import api_cnt
 
@@ -27,31 +26,53 @@ async def on_message(message: discord.message.Message):
     currentWorkLoop = asyncio.get_event_loop()
     if message.author == client.user:
         return
-    #if message.content.startswith('!d'):
-        #ctx = message.content.split(' ')[1]
-        #send = tools.xndmplus(ctx)
-        #if send:
-            #await message.channel.send(send)
+    if message.author.bot:
+        return
+    if message.guild.id == 1499348320861028442:
+        return
     if message.content.startswith('d66'):
-        #await message.channel.send(tools.d66())
-        pass #until fully sub
+        await message.channel.send(str(randint(1,6)) + str(randint(1,6)))
+        return
     
-    if message.content.startswith('!'):
-        ctx = message.content.split(' ',1)
-        sys,cmd = ctx[0].lstrip('!'), ctx[1]
-        
-        #await message.channel.send(sw.comment_parse(cmd))
-        ct = api_cnt.roll_dice(sys, cmd)
-        if ct:
-            await message.channel.send(ct)
+    if message.channel.id == 1508886760686620742:
+        await message.author.ban(delete_message_days= 1, reason='Hacked account.')
     
     if message.content.startswith('.'):
         pattern_inf = r"\.(\d+)wd(\d+)?"
-        match_inf = re.search(pattern_inf, message.content, re.IGNORECASE)
+        match_inf = re.match(pattern_inf, message.content, re.IGNORECASE)
         if match_inf:
-            ct = inf.roll_inf(message.content)
-            if ct:
-                await message.channel.send(ct)
+            ct = await inf.roll_inf(message.content)
+            await message.channel.send(ct)
+            return
+        ctx = message.content.split(' ',1)
+        sys,cmd = ctx[0].lstrip('.'), ctx[1]
+        
+        #await message.channel.send(sw.comment_parse(cmd)) abandon
+        ct = await api_cnt.roll_dice_async(sys, cmd)
+        if ct:
+            await message.channel.send(ct)
+            return
+        
+        
+    pattern_normal_d = r'(\d+)d(\d+)'
+    pattern_multiply_d = r'x(\d+)'
+    match_nd = re.match(pattern_normal_d, message.content, re.IGNORECASE)
+    match_md = re.match(pattern_multiply_d, message.content, re.IGNORECASE)
+    if match_nd or match_md:
+        ct = await api_cnt.roll_dice_async('sw', message.content)
+        if ct:
+            await message.channel.send(ct)
+        return
+    
+    if message.content.startswith('隨機'):
+        ct = message.content.split(' ')
+        ct.pop(0)
+        await message.channel.send(choice(ct))
+        return
+    
+
+        
+            
 
 
 token = os.environ.get('TOKEN')
